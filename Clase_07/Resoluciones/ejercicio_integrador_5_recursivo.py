12fa463f806d629c943ecdfb33f807b2b527ea05
Alejandro <alehunt@gmail.com>
2022-09-12 18:37:30 -0300

    Resoluciones de la clase 07

---
+++ Clase_07/Resoluciones/ejercicio_integrador_5_recursivo.py
@@ -0,0 +1,12 @@
+def get_int():
+    user_input = input('Por favor ingrese un número: ')
+    try:
+        value = int(user_input)
+    except ValueError:
+        print('No es un entero válido. Intente nuevamente!')
+        return get_int()
+    else:
+        return value
+
+
+print(f"Número ingresado: {get_int()}")
