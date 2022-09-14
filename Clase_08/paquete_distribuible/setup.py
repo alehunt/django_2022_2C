caba7e6231a434c569bbd70803febc85eddb628f
Alejandro <alehunt@gmail.com>
2022-09-14 18:32:37 -0300

    Clase_08: Paquetes en python

---
+++ Clase_08/paquete_distribuible/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name="recursos_humanos",
+    version="0.1",
+    description="Paquete de ejemplo de empleados y nómina",
+    author="Codo a Codo",
+    author_email="io.codoacodo@bue.edu.ar",
+    url="https://www.buenosaires.gob.ar/educacion/codo-codo",
+    packages=['recursos_humanos','recursos_humanos.personal'],
+    scripts=[]
+)
---
+++ Clase_08/utilizar_paquete.py
@@ -0,0 +1,13 @@
+from recursos_humanos.nomina import Nomina
+from recursos_humanos.personal.empleados import *
+
+nomina_empleados = Nomina()
+# pratto = EmpleadoFullTime('Lucas', 'Pratto', 6000)
+# nomina_empleados.agregar_empleado(pratto)
+nomina_empleados.agregar_empleado(EmpleadoFullTime('Lucas', 'Pratto', 6000))
+nomina_empleados.agregar_empleado(EmpleadoFullTime('Lucas', 'Janson', 6500))
+nomina_empleados.agregar_empleado(EmpleadoPorHora('Valentin', 'Gomez', 200, 50))
+nomina_empleados.agregar_empleado(EmpleadoPorHora('Walter', 'Bou', 150, 100))
+nomina_empleados.agregar_empleado(EmpleadoPorHora('Santi', 'Caseres', 100, 150))
+
+nomina_empleados.print()
