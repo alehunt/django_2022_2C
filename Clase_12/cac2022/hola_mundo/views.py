04a39d63e4760664a496d95cbfbebfdd31c8bda0
Alejandro <alehunt@gmail.com>
2022-10-05 11:37:19 -0300

    Subidos cambios sin subir hechos en distintas clases

--- Clase_12/cac2022/hola_mundo/views.py
+++ Clase_12/cac2022/hola_mundo/views.py
@@ -1,14 +1,13 @@
 from datetime import datetime
-from django.template import loader
-from django.http import HttpResponse
-# from django.shortcuts import render
+# from django.template import loader
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+# def index(request):
+#     template = loader.get_template('hola_mundo/index.html')
+#     context = {"hoy": datetime.now}
+#     return HttpResponse(template.render(context, request))
 
 
 def index(request):
-    template = loader.get_template('hola_mundo/index.html')
-    context = {"hoy": datetime.now}
-    return HttpResponse(template.render(context, request))
-
-
-# def index(request):
-#     return render(request, "hola_mundo/index.html", {"hoy": datetime.now})
+    return render(request, "hola_mundo/index.html", {"hoy": datetime.now})
