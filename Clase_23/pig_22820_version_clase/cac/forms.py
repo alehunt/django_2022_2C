70bb083f71419e6ff26f58cc6e80785c7f40ea6c
Alejandro <alehunt@gmail.com>
2022-11-07 18:30:11 -0300

    Clase_23: Material y ejemplo

---
+++ Clase_23/pig_22820_version_clase/cac/forms.py
@@ -0,0 +1,132 @@
+from django import forms
+from django.forms import ValidationError
+from .models import Estudiante
+
+
+def solo_caracteres(value):
+    if any(char.isdigit() for char in value):
+        raise ValidationError('El nombre no puede contener números. %(valor)s',
+                              code='Error1',
+                              params={'valor': value})
+        #raise ValidationError('El nombre no puede contener números.')
+
+
+class ContactoForm(forms.Form):
+
+    TIPO_CONSULTA = (
+        ('', '-Seleccione-'),
+        (1, 'Inscripciones'),
+        (2, 'Soporte Aula Virtual'),
+        (3, 'Ser docente'),
+    )
+
+    nombre = forms.CharField(
+        label='Nombre',
+        required=False,
+        validators=(solo_caracteres,),
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Ingrese solo texto'})
+    )
+    email = forms.EmailField(
+        label='Email',
+        max_length=50,
+        error_messages={
+            'required': 'Por favor completa el campo',
+        },
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'type': 'email'})
+    )
+    asunto = forms.CharField(
+        label='Asunto',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    mensaje = forms.CharField(
+        label='Mensaje',
+        max_length=500,
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    suscripcion = forms.BooleanField(
+        label='Deseo suscribirme a las novedades de codo a codo',
+        required=False,
+        widget=forms.CheckboxInput(
+            attrs={'class': 'form-check-input', 'value': 1})
+    )
+
+    tipo_consulta = forms.ChoiceField(
+        label='Tipo de consulta',
+        choices=TIPO_CONSULTA,
+        initial='2',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    def clean_mensaje(self):
+        data = self.cleaned_data['mensaje']
+        if len(data) < 10:
+            raise ValidationError(
+                "Debes especificar mejor el mensaje que nos envias")
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+        asunto = cleaned_data.get("asunto")
+        suscripcion = cleaned_data.get("suscripcion")
+
+        if suscripcion and asunto and "suscripcion" not in asunto:
+            msg = "Debe agregar la palabara 'suscripcion' al asunto."
+            self.add_error('asunto', msg)
+
+
+# class EstudianteForm(forms.ModelForm):
+#     class Meta:
+#         model = Estudiante
+#         fields = '__all__'
+
+class EstudianteForm(forms.ModelForm):
+    nombre = forms.CharField(error_messages={'required': 'PONE EL VALOOR'})
+
+    class Meta:
+        model = Estudiante
+        fields = '__all__'    
+        widgets = {
+            'apellido': forms.Textarea(attrs={'cols': 20, 'rows': 20}),
+        }
+        error_messages = {
+            'email': {
+                'required': 'Y EL VALOR QUE ONDA?',
+            },
+        }
+
+
+class EstudianteFormValidado(EstudianteForm):
+    def clean_apellido(self):
+        apellido = self.cleaned_data['apellido']
+        if apellido.upper() == "MESSI":
+            raise ValidationError("Messi no es estudiante")
+
+        return apellido
+
+# class EstudianteForm(forms.Form):
+#     nombre = forms.CharField(
+#         label='Nombre:',
+#         required=True,
+#         validators=(solo_caracteres,),
+#         widget=forms.TextInput(
+#             attrs={'class': 'form-control', 'placeholder': 'Ingrese solo texto'})
+#     )
+#     apellido = forms.CharField(
+#         label='Apellido:',
+#         required=True,
+#         validators=(solo_caracteres,),
+#         widget=forms.TextInput(
+#             attrs={'class': 'form-control', 'placeholder': 'Ingrese solo texto'})
+#     )
+#     email = forms.EmailField(
+#         label='Email:',
+#         max_length=50,
+#         error_messages={
+#             'required': 'Por favor completa el campo',
+#         },
+#         widget=forms.TextInput(
+#             attrs={'class': 'form-control', 'type': 'email'})
+#     )
+#     dni = forms.IntegerField(label="DNI:", error_messages={'required': 'Complete el DNI.', })
---
+++ Clase_23/pig_22820_version_clase/cac/views.py
@@ -0,0 +1,227 @@
+from datetime import datetime
+from multiprocessing import context
+from django.http import HttpResponse, JsonResponse
+
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from django.template import loader
+
+from cac.forms import ContactoForm, EstudianteForm, EstudianteFormValidado
+
+from django.contrib import messages
+
+from cac.models import Estudiante
+from django.views import View
+from django.views.generic import ListView
+
+def index(request):
+    listado_cursos = [
+        {
+            'nombre': 'Fullstack Java',
+            'descripcion': 'Curso de Fullstack',
+            'categoria': 'Programación'
+        },
+        {
+            'nombre': 'Diseño UX/IU',
+            'descripcion': '🎨',
+            'categoria': 'Diseño'
+        },
+        {
+            'nombre': 'Big Data',
+            'descripcion': 'test',
+            'categoria': 'Analisis de Datos'
+        },
+    ]
+
+    if (request.method == 'POST'):
+        contacto_form = ContactoForm(request.POST)
+        if (contacto_form.is_valid()):
+            # enviar un email al administrado con los datos
+            # guardar los datos en la base
+            messages.success(
+                request, 'Muchas gracias por contactarte, te esteremos respondiendo en breve.')
+            messages.info(request, 'Otro mensajito')
+            # deberia validar y realizar alguna accion
+        else:
+            messages.warning(request, 'Por favor revisa los errores')
+    else:
+        contacto_form = ContactoForm()
+
+    return render(request, 'cac/publica/index.html',
+                  {'cursos': listado_cursos, 'contacto_form': contacto_form})
+
+
+def quienes_somos(request):
+    # return redirect('saludar_por_defecto')
+    # return redirect(reverse('saludar', kwargs={'nombre':'Juliana'}))
+    template = loader.get_template('cac/publica/quienes_somos.html')
+    context = {'titulo': 'Codo a Codo - Quienes Somos'}
+    return HttpResponse(template.render(context, request))
+
+
+def ver_proyectos(request, anio=2022, mes=1):
+    proyectos = []
+    return render(request, 'cac/publica/proyectos.html', {'proyectos': proyectos})
+
+
+def ver_cursos(request):
+    listado_cursos = [
+        {
+            'nombre': 'Fullstack Java',
+            'descripcion': 'Curso de Fullstack',
+            'categoria': 'Programación'
+        },
+        {
+            'nombre': 'Diseño UX/IU',
+            'descripcion': '🎨',
+            'categoria': 'Diseño'
+        },
+        {
+            'nombre': 'Big Data',
+            'descripcion': 'test',
+            'categoria': 'Analisis de Datos'
+        },
+    ]
+    return render(request, 'cac/publica/cursos.html', {'cursos': listado_cursos})
+
+
+def api_proyectos(request,):
+    proyectos = [{
+        'autor': 'Gustavo Villegas',
+        'portada': 'https://agenciadeaprendizaje.bue.edu.ar/wp-content/uploads/2021/12/Gustavo-Martin-Villegas-300x170.png',
+        'url': 'https://marvi-artarg.web.app/'
+    }, {
+        'autor': 'Enzo Martín Zotti',
+        'portada': 'https://agenciadeaprendizaje.bue.edu.ar/wp-content/uploads/2022/01/Enzo-Martin-Zotti-300x170.jpg',
+        'url': 'https://hablaconmigo.com.ar/'
+    }, {
+        'autor': 'María Echevarría',
+        'portada': 'https://agenciadeaprendizaje.bue.edu.ar/wp-content/uploads/2022/01/Maria-Echevarria-300x170.jpg',
+        'url': 'https://compassionate-colden-089e8a.netlify.app/'
+    }, ]
+    response = {'status': 'Ok', 'code': 200,
+                'message': 'Listado de proyectos', 'data': proyectos}
+    return JsonResponse(response, safe=False)
+
+
+def index_administracion(request):
+    variable = 'test variable'
+    return render(request, 'cac/administracion/index_administracion.html', {'variable': variable})
+
+
+# Create your views here.
+def hola_mundo(request):
+    return HttpResponse('Hola Mundo Django')
+
+
+def saludar(request, nombre='Pepe'):
+    return HttpResponse(f"""
+        <h1>Hola Mundo Django - {nombre}</h1>
+        <p>Estoy haciendo mi primera prueba</p>
+    """)
+
+
+def ver_proyectos_2022_07(request):
+    return HttpResponse(f"""
+        <h1>Proyectos del mes 7 del año 2022</h1>
+        <p>Listado de proyectos</p>
+    """)
+
+
+def ver_proyectos_anio(request, anio):
+    return HttpResponse(f"""
+        <h1>Proyectos del  {anio}</h1>
+        <p>Listado de proyectos</p>
+    """)
+
+
+def cursos_detalle(request, nombre_curso):
+    return HttpResponse(f"""
+        <h1>{nombre_curso}</h1>
+    """)
+
+
+def cursos(request, nombre):
+    return HttpResponse(f"""
+        <h2>{nombre}</h2>
+    """)
+
+
+def estudiantes_index(request):
+    estudiantes = Estudiante.objects.all().order_by('dni')
+    return render(request, 'cac/administracion/estudiantes/index.html', {'estudiantes': estudiantes})
+
+
+def estudiantes_nuevo(request):
+    if request.method == "POST":
+        formulario = EstudianteForm(request.POST)
+        if formulario.is_valid():
+            nombre = formulario.cleaned_data['nombre']
+            apellido = formulario.cleaned_data['apellido']
+            dni = formulario.cleaned_data['dni']
+            email = formulario.cleaned_data['email']
+            nuevo_estudiante = Estudiante(nombre=nombre, apellido=apellido, email=email, dni=dni)
+
+            try:
+                nuevo_estudiante.save()
+            except ValueError as ve:
+                formulario.add_error('apellido', str(ve))
+            else:
+                return redirect('estudiantes_index')
+    else:
+        formulario = EstudianteForm()
+    return render(request, 'cac/administracion/estudiantes/nuevo.html', {'formulario': formulario})
+
+
+# class EstudiantesListView(ListView):
+#     model = Estudiante
+#     context_object_name = 'estudiantes'
+#     template_name = 'cac/administracion/estudiantes/index.html'
+#     ordering = ['dni']
+
+# class EstudiantesView(View):
+#     form_class = EstudianteFormValidado
+#     template_name = 'cac/administracion/estudiantes/nuevo.html'
+
+#     def get(self, request, *args, **kwargs):
+#         form = self.form_class()
+#         return render(request, self.template_name, {'formulario': form})
+
+#     def post(self, request, *args, **kwargs):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('estudiantes_index')
+
+#         return render(request, self.template_name, {'formulario': form})
+
+
+def estudiantes_editar(request, id_estudiante):
+    try:
+        estudiante = Estudiante.objects.get(id=id_estudiante)
+    except Estudiante.DoesNotExist:
+        return render(request, 'cac/administracion/404_admin.html')
+    
+    if request.method == "POST":
+        formulario = EstudianteFormValidado(request.POST, instance=estudiante)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('estudiantes_index')
+    else:
+        formulario = EstudianteFormValidado(instance=estudiante)
+
+    return render(request, 'cac/administracion/estudiantes/editar.html', {'formulario': formulario, 'id_estudiante': id_estudiante})
+
+
+def estudiantes_eliminar(request, id_estudiante):
+    try:
+        estudiante = Estudiante.objects.get(id=id_estudiante)
+    except Estudiante.DoesNotExist:
+        return render(request, 'cac/administracion/404_admin.html')
+    
+    try:
+        estudiante.delete()
+    except ValueError as ve:
+        messages.error(request=request, message="Almada necesita seguir siendo estudiante")
+    return redirect('estudiantes_index')
