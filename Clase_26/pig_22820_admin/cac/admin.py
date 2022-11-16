42d7d37f6170bcdf191b186ce5c3b67f1518fc07
Alejandro <alehunt@gmail.com>
2022-11-16 19:33:27 -0300

    Clase_26: Modificaciones hechas en clase

--- Clase_26/pig_22820_admin/cac/admin.py
+++ Clase_26/pig_22820_admin/cac/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from cac.models import Estudiante
+from cac.models import Estudiante, Comision
 
-admin.site.register(Estudiante)
+
+class EstudianteAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Estudiante, EstudianteAdmin)
+# admin.site.register(Comision)
+
+@admin.register(Comision)
+class ComisionAdmin(admin.ModelAdmin):
+    pass
+
 
--- Clase_26/pig_22820_admin/cac/models.py
+++ Clase_26/pig_22820_admin/cac/models.py
@@ -4,11 +4,31 @@ from tabnanny import verbose
 from django.db import models
 
 
+class Comision(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name='Comsion:')
+    imagen = models.ImageField(verbose_name="Imagen:", default=None, null=True)
+
+    class Meta:
+        verbose_name_plural = "Comisiones"
+
+    def __str__(self):
+        return self.nombre
+
+    def delete(self):
+        self.imagen.delete()
+        return super().delete()
+
+
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=100, verbose_name='Nombre:')
     apellido = models.CharField(max_length=150, verbose_name='Apellido:')
-    email = models.EmailField(max_length=150, verbose_name='Email:', null=True, default=None)
+    email = models.EmailField(
+        max_length=150, verbose_name='Email:', null=True, default=None)
     dni = models.BigIntegerField(verbose_name='DNI:')
+    comision = models.ForeignKey(Comision, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name_plural = "Estudiantes"
 
     def __str__(self):
         return f"DNI: {self.dni} - {self.apellido}, {self.nombre}"
@@ -23,6 +43,3 @@ class Estudiante(models.Model):
         if self.apellido.upper() == 'ALMADA':
             raise ValueError("No se puede eliminar, tiene mucho que aprender")
         return super().delete(*args, **kwargs)
-
-
-
